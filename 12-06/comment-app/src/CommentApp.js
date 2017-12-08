0863c516f5466d690bf0d617388f5050e135a7e6
XieHaiTao <229760995@qq.com>
2017-12-08 12:20:50 +0800

    用react实现了todolist,结合高阶组件优化

--- 12-06/comment-app/src/CommentApp.js
+++ 12-06/comment-app/src/CommentApp.js
@@ -2,30 +2,19 @@ import React,{Component} from 'react';
 import CommentInput from './CommentInput';
 import CommentList from './CommentList';
 import PropTypes from 'prop-types'
+import wrapWithLoadData from './wrapWithLoadData';
 
 class CommentApp extends Component{
-    componentWillMount(){
-        this._loadComments();
-    }
-
-    _loadComments(){
-        let comments = localStorage.getItem('comments');
-        if(comments){
-            comments = JSON.parse(comments);
-            this.setState({comments});
-        }
-    }
-
-    _saveComments(comments){
-        localStorage.setItem('comments',JSON.stringify(comments));
+    static propTypes = {
+        data:PropTypes.any,
+        saveData:PropTypes.func.isRequired
     }
 
 
-
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
         this.state = {
-            comments :[]
+           comments:props.data
         }
     }
 
@@ -40,7 +29,7 @@ class CommentApp extends Component{
         this.setState({
             comments
         })
-        this._saveComments(comments)
+        this.props.saveData(comments)
     }
 
     handleDeleteComment = (index)=>{
@@ -48,7 +37,7 @@ class CommentApp extends Component{
         const comments = this.state.comments;
         comments.splice(index,1);
         this.setState({comments});
-        this._saveComments(comments);
+        this.props.saveData(comments);
     }
 
     render(){
@@ -65,5 +54,6 @@ class CommentApp extends Component{
     }
 }
 
+CommentApp = wrapWithLoadData(CommentApp,'comments');
 
 export default CommentApp;  //  导出组件
